96b36e232f49146f1df9b4afe14d0af19225795b
Samuel Faber <samuel.faber@live.fr>
2023-11-09 09:42:51 +0100

    Appel API avec axios avec un console.log de notre réponse

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API = "https://api-pokemon-fr.vercel.app/api/v1/gen/2";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  axios
+    .get(API)
+    .then((res) => console.log(res.data[0].name.fr))
+    .catch((error) => console.error(error));
+
+  return <></>;
 }
 
-export default App
+export default App;
