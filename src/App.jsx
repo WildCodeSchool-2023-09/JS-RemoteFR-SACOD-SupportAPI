fc05b53690e777009f429eba1d02aab3f81714cd
Samuel Faber <samuel.faber@live.fr>
2023-11-09 10:14:38 +0100

    Code à la fin du support

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const API = "https://api-pokemon-fr.vercel.app/api/v1/gen/2";
+  const API = "https://api-pokemon-fr.vercel.app/api/v1/gen/";
 
-  axios
-    .get(API)
-    .then((res) => console.log(res.data[0].name.fr))
-    .catch((error) => console.error(error));
+  const [pokemons, setPokemons] = useState([]);
 
-  return <></>;
+  const [generation, setGeneration] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`${API}${generation}`)
+      .then((res) => setPokemons(res.data))
+      .catch((error) => console.error(error));
+  }, [generation]);
+
+  if (pokemons.length === 0) {
+    return <p>En cours de chargement...</p>;
+  }
+
+  console.log(pokemons);
+
+  return (
+    <main>
+      <button onClick={() => setGeneration(generation + 1)}>
+        Generation suivante
+      </button>
+      {pokemons.map((pokemon) => (
+        <div key={pokemon.pokedexId}>
+          <img
+            src={pokemon.sprites.regular}
+            alt={pokemon.name.fr}
+            width={256}
+          />
+          <p>{pokemon.name.fr}</p>
+        </div>
+      ))}
+    </main>
+  );
 }
 
 export default App;
